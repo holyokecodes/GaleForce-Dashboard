8ed28e97769464afb5a8f0a7bd749b421beb7b5c
Andrew Pasquale <andrew@elytra.net>
2022-02-22 16:06:57 -0500

    Add description to landing page

--- pages/index.vue
+++ pages/index.vue
@@ -19,11 +19,34 @@
       mx="auto"
       class="content">
       <Sessions v-if="user"/>
-      <c-box v-if="!user" py="10">
-        <c-text text-align="center">
-          Login with a Google account to manage your GaleForce game sessions.
-        </c-text>
-      </c-box>
+      <c-grid v-if="!user" w="100%" h="200" gap="6" p="10" template-columns="repeat(3, 1fr)" template-rows="repeat(2, 1fr)">
+        <c-grid-item col-span="3" h="10">
+          <c-text text-align="center" mb="2">
+            Login with a Google account.
+          </c-text>
+        </c-grid-item>
+        <c-grid-item col-span="2" >
+          <c-text mb="2">
+            The GaleForce dashboard allows you to create sessions that players can join. To play with a group, create a session and share the 6 digit code with all the players.
+          </c-text>
+          <c-text mb="2">
+            GaleForce missions are organized into stages: 
+            <ul>
+              <li><em>Training stages</em> help players learn coding concepts and the capabilities of the robots.</li>
+              <li><em>Preparation stages</em> happen before the storm.</li>
+              <li><em>Response stages</em> after the storm hits.</li>
+            </ul>
+            The dashboard allows you to select the active stage of the game for each team.
+          </c-text>
+          <c-text mb="2">
+            The dashboard allows you to see which missions have been completed and reset mission completion status if needed. Other configuration options include mute all players, select instructional or motivational videos to play, and more.
+          </c-text>
+          </c-grid-item>
+        <c-grid-item>
+          <video autoplay="" loop="" playsinline="" muted=""><source src="https://galeforcegame.com/wp-content/uploads/2021/11/galeforce.m4v"></video>
+        </c-grid-item>
+      </c-grid>
+      
   </c-box>
   </div>
 </template><script>
@@ -36,4 +59,7 @@ export default {
 }
 </script>
 <style>
+li {
+  margin-left: 20px;
+}
 </style>
